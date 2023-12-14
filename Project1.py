dcfa79c32a732dd254ddb985defc3f4ff6b49d44
Farhaan Khan <kfarhaan@gmail.com>
2023-12-13 21:15:49 -0800

    Add files via upload

---
+++ Project1.py
@@ -0,0 +1,36 @@
+'''
+base list
+base input = number
+while loop
+    new input
+    append input to list
+    break at number
+average function
+for each item in list
+    if item is greater than average
+    count += 1
+number of days above average
+print
+'''
+
+days_temp = list()
+days = int(input("How many day's temperature? "))
+value = 0
+count = 0
+
+while(True):
+    count = len(days_temp) + 1
+    if count <= days:
+        temp = int(input(f"Day {count}'s high temp: "))
+        days_temp.append(temp)
+    else: break
+
+average_temp = sum(days_temp) / count
+print('Average = ', average_temp)
+
+highest = 0
+for d in days_temp:
+    if int(d) >= average_temp:
+        highest += 1
+
+print(f'{highest} day(s) above average')
